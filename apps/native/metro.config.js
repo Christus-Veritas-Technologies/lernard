f71849dccd459ff86aa737a01bc1e8fc6cc9495b
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-05-12 17:18:09 +0200

    fix: resolve Node 22 ESM loader Windows path issue in metro config
    - Use async IIFE to defer path resolution, working around ESM loader constraint
    - Metro bundler now starts successfully on Windows with Expo 54
    - NativeWind integration preserved

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -1,12 +1,10 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const path = require('path');
+﻿// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-
 const { withNativeWind } = require('nativewind/metro');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-
-const projectRoot = path.resolve(__dirname);
-const config = getDefaultConfig(projectRoot);
-
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = (async () => {
+  return withNativeWind(
+    await getDefaultConfig(__dirname),
+    { input: './global.css' }
+  );
+})();
